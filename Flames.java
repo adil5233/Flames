8192924d91a97964508663ae6aa7a29623d5650a
Adil <adil.amin820@gmail.com>
2021-02-25 01:15:53 +0530

    more optimizations cuz why not :p

--- Flames.java
+++ Flames.java
@@ -6,7 +6,35 @@ public class Flames {
         System.out.println("Enter the string1  : ");
         String s1 = sc.nextLine();System.out.println("Enter the String2 : ");
         String s2 = sc.nextLine();
-        int length = s1.length()+s2.length();int count = 0;
+        flamesCount(s1,s2);
+        flamesResult(flamesCount(s1,s2));
+
+    }
+
+    private static void flamesResult(int flamesCount) {
+        String flames = "flames";
+        int position;
+        String temp;
+        while (flames.length() != 1)
+        {
+            position = (flamesCount) % flames.length();
+            if ( position != 0)
+            {
+                temp = flames.substring(position)+flames.substring(0,position-1);
+            }
+            else
+            {
+                temp = flames.substring(0,flames.length()-1);
+            }
+            flames = temp;
+        }
+        System.out.println(flames);
+
+    }
+
+    private static int flamesCount(String s1, String s2) {
+        int length = s1.length()+s2.length();
+        int count = 0;
         for (int i = 0;i<s1.length();i++)
         {
             for (int j = 0;j<s2.length();j++)
@@ -18,25 +46,6 @@ public class Flames {
                 }
             }
         }
-        int total = length - (count*2);
-        
-        String flames = "flames";
-        int position;
-        String temp;
-        while (flames.length() != 1)
-        {
-            position = (total) % flames.length();
-            if ( position != 0)
-            {
-                temp = flames.substring(position)+flames.substring(0,position-1);
-            }
-            else
-                {
-                    temp = flames.substring(0,flames.length()-1);
-                }
-                flames = temp;
-            }
-        System.out.println(flames);
-
+        return length - (count*2);
     }
 }
