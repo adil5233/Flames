191b8f8e33ca54b85e00d3f0713a7a5400d5395a
Adil <adil.amin820@gmail.com>
2021-02-25 01:07:07 +0530

    fix errors and add some optmizations

--- Flames.java
+++ Flames.java
@@ -1,50 +1,42 @@
 import java.util.Scanner;
 
 public class Flames {
-public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the string1  : ");
-	String s1 = sc.nextLine();System.out.println("Enter the String2 : ");
-	String s2 = sc.nextLine();
-	int length = s1.length()+s2.length();int count = 0;
-	for (int i = 0;i<s1.length();i++)
-	{
-		for (int j = 0;j<s2.length();j++)
-		{
-			if(s1.charAt(i) == s2.charAt(j))
-			{
-				count = count +1;
-				break;
-			}
-		}
-	}
-	int total = length - (count*2);
-	System.out.println(total);
-	
-	public static void main(String[] args) {
-	String s1 = "flames";
-	int position;String temp;
-	while (s1.length() != 1)
-	{
-		position = (total) % s1.length();
-		if ( position != 0)
-		{
-			temp = s1.substring(position)+s1.substring(0,position-1);
-			System.out.println(temp);
-		}
-		else
-		{
-			temp = s1.substring(0,s1.length()-1);
-		}
-		s1 = temp;
-	}
-	System.out.println(s1);
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string1  : ");
+        String s1 = sc.nextLine();System.out.println("Enter the String2 : ");
+        String s2 = sc.nextLine();
+        int length = s1.length()+s2.length();int count = 0;
+        for (int i = 0;i<s1.length();i++)
+        {
+            for (int j = 0;j<s2.length();j++)
+            {
+                if(s1.charAt(i) == s2.charAt(j))
+                {
+                    count = count +1;
+                    break;
+                }
+            }
+        }
+        int total = length - (count*2);
+        
+        String flames = "flames";
+        int position;
+        String temp;
+        while (flames.length() != 1)
+        {
+            position = (total) % flames.length();
+            if ( position != 0)
+            {
+                temp = flames.substring(position)+flames.substring(0,position-1);
+            }
+            else
+                {
+                    temp = flames.substring(0,flames.length()-1);
+                }
+                flames = temp;
+            }
+        System.out.println(flames);
 
-
-
-
-	
-	
-}
+    }
 }
